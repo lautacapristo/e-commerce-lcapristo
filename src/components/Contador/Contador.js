2ef1cc50ee37f141c8f0669cd55572bad2ba2848
Lautaro Capristo <lautarocapristo@outlook.com>
2021-12-01 16:42:21 -0300

    contador2

--- src/components/Contador/Contador.js
+++ src/components/Contador/Contador.js
@@ -8,11 +8,17 @@ const [ count, setCount ] = useState(0)
 const handler=() => {
     setCount(count+1)
 }
+const handler2=() => {
+    setCount(count-1)
+}
 
     return (
         <div> 
 <button onClick={handler}>Sumar + </button>
+<button onClick={handler2}>Restar - </button>
+
 <p>{count} </p>
+
     </div>
     )
 }
